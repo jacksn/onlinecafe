055d124b4246fb689773ac7cbd848b864321fb5d
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-08-19 13:30:53 +0300

    Add automatic i18n initialization

--- src/main/java/test/onlinecafe/config/AppConfiguration.java
+++ src/main/java/test/onlinecafe/config/AppConfiguration.java
@@ -1,9 +1,13 @@
 package test.onlinecafe.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
@@ -14,17 +18,24 @@ import java.util.Locale;
 })
 public class AppConfiguration {
 
-    @Value("${app.i18n.supported_languages}")
-    private String supportedLanguages;
+    @Autowired
+    Environment environment;
 
     @Bean(name = "messageSource")
-    public ResourceBundleMessageSource resourceBundleMessageSource() {
+    public ResourceBundleMessageSource resourceBundleMessageSource(List<String> supportedLanguages) {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages/app");
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setFallbackToSystemLocale(false);
         messageSource.setUseCodeAsDefaultMessage(true);
-        Locale.setDefault(Locale.forLanguageTag(supportedLanguages.split(",")[0]));
+        Locale.setDefault(Locale.forLanguageTag(supportedLanguages.get(0)));
         return messageSource;
+    }
+
+    @Bean(name = "supportedLanguages")
+    List<String> supportedLanguages() {
+        List<String> supportedLanguages = new ArrayList<>();
+        supportedLanguages.addAll(Arrays.asList(environment.getProperty("app.i18n.supported_languages").split(",")));
+        return supportedLanguages;
     }
 }
