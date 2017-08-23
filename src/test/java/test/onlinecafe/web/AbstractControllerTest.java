4f02c668c7557123b1cab17825cc5ceaa904ef1d
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-08-23 21:08:35 +0300

    Add resource controller test

---
+++ src/test/java/test/onlinecafe/web/AbstractControllerTest.java
@@ -0,0 +1,44 @@
+package test.onlinecafe.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootTest
+@ActiveProfiles("repo-jpa, discount-none")
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@TestPropertySource("classpath:db/db_hsqldb.properties")
+public abstract class AbstractControllerTest {
+    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+
+    static {
+        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
+        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    }
+
+    protected MockMvc mockMvc;
+
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @PostConstruct
+    private void postConstruct() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilter(CHARACTER_ENCODING_FILTER)
+                .build();
+    }
+
+}
---
+++ src/test/java/test/onlinecafe/web/ResourceControllerTest.java
@@ -0,0 +1,17 @@
+package test.onlinecafe.web;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+    @Test
+    public void testResources() throws Exception {
+        mockMvc.perform(get("/css/style.css"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("navbar")));
+    }
+}
