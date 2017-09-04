35f470a86db3fe5d712de5216ad504137437e642
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-09-04 16:54:49 +0300

    Add controller tests

--- src/test/java/test/onlinecafe/web/CoffeeControllerTest.java
+++ src/test/java/test/onlinecafe/web/CoffeeControllerTest.java
@@ -4,20 +4,25 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.context.MessageSource;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import test.onlinecafe.CoffeeTypeTestData;
+import test.onlinecafe.CoffeeOrderTestData;
 import test.onlinecafe.dto.CoffeeTypeDtoListWrapper;
 import test.onlinecafe.dto.Notification;
 import test.onlinecafe.dto.NotificationType;
+import test.onlinecafe.model.CoffeeOrder;
+import test.onlinecafe.model.CoffeeOrderItem;
+import test.onlinecafe.model.CoffeeType;
 import test.onlinecafe.service.CoffeeOrderService;
 import test.onlinecafe.service.CoffeeTypeService;
 import test.onlinecafe.service.ConfigurationService;
-import test.onlinecafe.util.CoffeeOrderUtil;
 import test.onlinecafe.util.discount.MockDiscount;
 
 import static org.hamcrest.CoreMatchers.isA;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static test.onlinecafe.CoffeeTypeTestData.COFFEE_TYPES_ENABLED;
 import static test.onlinecafe.web.CoffeeController.*;
@@ -26,6 +31,8 @@ public class CoffeeControllerTest extends AbstractControllerTest {
     @InjectMocks
     private CoffeeController coffeeController;
 
+    @Mock
+    private MessageSource messageSource;
     @Mock
     private CoffeeOrderService orderService;
     @Mock
@@ -55,8 +62,47 @@ public class CoffeeControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void postRootTest() throws Exception {
-        CoffeeTypeDtoListWrapper typeDtoList = CoffeeOrderUtil.getCoffeeTypeDtoList(CoffeeTypeTestData.COFFEE_TYPES_ENABLED);
+    public void postValidDataToRootTest() throws Exception {
+        CoffeeOrder order = CoffeeOrderTestData.getCoffeeOrder2();
+        for (CoffeeOrderItem orderItem : order.getOrderItems()) {
+            CoffeeType type = orderItem.getCoffeeType();
+            when(typeService.get(type.getId())).thenReturn(type);
+        }
 
+        mockMvc.perform(
+                post("/")
+                        .param("content-type", "application/x-www-form-urlencoded")
+                        .param("coffeeTypeDtos[0].typeId", "2")
+                        .param("coffeeTypeDtos[0].selected", "true")
+                        .param("coffeeTypeDtos[0].quantity", "2")
+                        .param("coffeeTypeDtos[1].typeId", "4")
+                        .param("coffeeTypeDtos[1].selected", "true")
+                        .param("coffeeTypeDtos[1].quantity", "1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("order"));
     }
+
+    @Test
+    public void postListWithZeroQuantityToRootTest() throws Exception {
+        CoffeeOrder order = CoffeeOrderTestData.getCoffeeOrder2();
+        for (CoffeeOrderItem orderItem : order.getOrderItems()) {
+            CoffeeType type = orderItem.getCoffeeType();
+            when(typeService.get(type.getId())).thenReturn(type);
+        }
+        final String emptyOrder = "EMPTY ORDER";
+        when(messageSource.getMessage(eq(MESSAGE_ERROR_EMPTY_ORDER), eq(null), any())).thenReturn(emptyOrder);
+        mockMvc.perform(
+                post("/")
+                        .param("content-type", "application/x-www-form-urlencoded")
+                        .param("coffeeTypeDtos[0].typeId", "2")
+                        .param("coffeeTypeDtos[0].selected", "true")
+                        .param("coffeeTypeDtos[0].quantity", "0")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute(MODEL_ATTR_NOTIFICATION, new Notification(NotificationType.ERROR, emptyOrder)));
+    }
+
 }
