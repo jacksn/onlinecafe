a43419e8e45da6fb6ee5a2dc36270f6a77783793
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-09-04 11:39:29 +0300

    Override equals, hashcode and toString methods for testing purposes

--- src/main/java/test/onlinecafe/dto/Notification.java
+++ src/main/java/test/onlinecafe/dto/Notification.java
@@ -18,4 +18,30 @@ public class Notification implements Serializable {
     public String getMessage() {
         return message;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Notification that = (Notification) o;
+
+        if (type != that.type) return false;
+        return message != null ? message.equals(that.message) : that.message == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type != null ? type.hashCode() : 0;
+        result = 31 * result + (message != null ? message.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "type=" + type +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
